05b5811229ea9f4a5b71a70a609166650f3583d6
Nikolaas <nikotomad@gmail.com>
2018-09-28 12:50:27 +0200

    Added nodemon as dev-dependency so we don't have to manually restart server

--- server.js
+++ server.js
@@ -17,15 +17,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 // Routes
 app.use('/api', apiRouter);
 
-// We could create a new home.routes.js and home.controller.js if we had more functionalities
+//@INFO: could create a new home.routes.js and home.controller.js if we had more functions
 app.get('/', function(req, res){ res.sendFile(process.cwd() + '/views/index.html') });
 
-app.get('*', function (req, res) { // 404 Not found
+ // 404 Not found
+app.get('*', function (req, res) {
   res.status(404).json({
     'api_error': 'Endpoint not found'
   })
 });
 
-var listener = app.listen(port, function () {
+const listener = app.listen(port, function () {
   console.info('Node.js listening on port: ' + listener.address().port);
 });
